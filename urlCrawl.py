1034da9ddaea7ebc5cd2605c3111e14356755b0f
toymakercody <cody@archonweb.com>
2015-04-09 21:37:42 -0500

    Now makes a folder and saves page.
    This is by far no where near clean enough for my taste but I will save
    cleaning it up for tomorrows task.

--- urlCrawl.py
+++ urlCrawl.py
@@ -1,7 +1,14 @@
 import urllib.request
+import os
 url_page = input('Select your web page: ')
 if 'https://' not in url_page : url_page = 'https://%s' % url_page
 req = urllib.request.Request(url_page)
 response = urllib.request.urlopen(req)
 the_page = response.read()
-print(the_page)
+domain_name = url_page.split('.')
+domain_name = domain_name[0].split('https://')
+if not os.path.exists(domain_name[1]):
+    os.makedirs(domain_name[1])
+output = open(domain_name[1] + '\\' + domain_name[1] + '.html','wb')
+output.write(the_page)
+output.close()
