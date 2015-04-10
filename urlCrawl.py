05f959423ceedcfaa2fb658befcbdaaf035f2bf1
codebycody <cody.greene@bluenrgy.com>
2015-04-09 21:59:24 -0500

    Revert "Revert "swapped split for partition method""
    This reverts commit 4540f541bde6a2f491e57ef8da2432d31cb14819.

--- urlCrawl.py
+++ urlCrawl.py
@@ -1,14 +1,13 @@
 import urllib.request
 import os
 url_page = input('Select your web page: ')
+domain_name = url_page.partition('.')[0]
 if 'https://' not in url_page : url_page = 'https://%s' % url_page
 req = urllib.request.Request(url_page)
 response = urllib.request.urlopen(req)
 the_page = response.read()
-domain_name = url_page.split('.')
-domain_name = domain_name[0].split('https://')
-if not os.path.exists(domain_name[1]):
-    os.makedirs(domain_name[1])
-output = open(domain_name[1] + '\\' + domain_name[1] + '.html','wb')
+if not os.path.exists(domain_name):
+	os.makedirs(domain_name)
+output = open(domain_name + '\\' + domain_name + '.html','wb')
 output.write(the_page)
 output.close()
