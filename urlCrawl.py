9808d05fc7b05c384e2ca7b1d3d8245d73b8eff8
codebycody <cody.greene@bluenrgy.com>
2015-04-11 23:50:03 -0500

    create css folder
    needs to be made into a loop so it can be used for style and script tags

--- urlCrawl.py
+++ urlCrawl.py
@@ -9,8 +9,16 @@ response = urllib.request.urlopen(req)
 the_page = response.read()
 if not os.path.exists(domain_name):
 	os.makedirs(domain_name)
-print(url_page)
 #in the next round need to check the file name that browser gives.
 output = open(domain_name + '\\' + 'index' + '.html','wb')
+output.write(the_page)
+output.close()
+
+
+#needs to be made into a loop so it can be used for style and script tags
+if not os.path.exists(domain_name + '\\' + 'css'):
+	os.makedirs(domain_name + '\\' + 'css')
+#in the next round need to check the file name that browser gives.
+output = open(domain_name + '\\' + 'css' + '\\' + 'main' + '.css','wb')
 output.write(the_page)
 output.close()
