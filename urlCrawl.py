540caa8358fbbd2bf48c00d6881cc003a95857e9
codebycody <cody.greene@bluenrgy.com>
2015-09-14 22:33:54 -0400

    changed folder structure
    Now all the site data will be grouped into a single site folder and then
    stored in a folder with the sites url for better storage.

--- urlCrawl.py
+++ urlCrawl.py
@@ -2,27 +2,35 @@ import urllib.request
 import os
 url_page = input('Select your web page: ')
 domain_name = url_page.partition('.')[0]
+site_folder = 'sites' + '\\' + domain_name
 #! Needs a better way to check for http || https
 if 'https://' not in url_page : url_page = 'https://%s' % url_page
 
 req = urllib.request.Request(url_page)
 response = urllib.request.urlopen(req)
 the_page = response.read()
-if not os.path.exists(domain_name):
-	os.makedirs(domain_name)
+
+
+# check for the folder to put all the sites in if it doesn't exist make it
+if not os.path.exists('sites'):
+    os.makedirs('sites')
+
+
+if not os.path.exists(site_folder):
+	os.makedirs(site_folder)
 
 #in the next round need to check the file name that browser gives.
-output = open(domain_name + '\\' + 'index' + '.html','wb')
+output = open(site_folder + '\\' + 'index' + '.html','wb')
 output.write(the_page)
 output.close()
 
 
 #needs to be made into a loop so it can be used for style and script tags
-if not os.path.exists(domain_name + '\\' + 'css'):
-	os.makedirs(domain_name + '\\' + 'css')
+if not os.path.exists(site_folder + '\\' + 'css'):
+	os.makedirs(site_folder + '\\' + 'css')
 
 #in the next round need to check the file name that browser gives.
-output = open(domain_name + '\\' + 'css' + '\\' + 'main' + '.css','wb')
+output = open(site_folder + '\\' + 'css' + '\\' + 'main' + '.css','wb')
 output.write(the_page)
 output.close()
 
